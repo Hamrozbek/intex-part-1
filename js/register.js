1839963efb1e4b99470badaf33142a965e7d5d1a
Hamrozbek <hamroznuriddinov41@gmail.com>
2025-06-04 14:03:42 +0500

    salom

---
+++ js/register.js
@@ -0,0 +1,24 @@
+let elForm = document.querySelector(".register-form")
+
+elForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    let data = {
+        id: passwords.length + 1,
+        username: e.target.username.value,
+        password: e.target.password.value
+    }
+
+    passwords.push(data)
+    localStorage.setItem("passwords", JSON.stringify(passwords))
+
+    elForm.lastElementChild.previousElementSibling.innerHTML = `
+    <img class="w-[30px] h-[30px] scale-[1.6] mx-auto" src="./images/louding.png" alt="Loading..." width="30" height="30">
+  `
+
+    setTimeout(() => {
+        elForm.lastElementChild.previousElementSibling.innerHTML = 'Регистрация'
+        setTimeout(() => {
+            location.pathname = "/index.html"
+        }, 600)
+    }, 1000)
+})
